9c452eebad729fbda4628fbc6fcd0c58409b6194
JoaoSiberi <joaogabrielsiberi3@gmail.com>
2025-04-22 19:15:53 +0000

    Exercício Mestre Concluído

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,71 @@
 #include <stdio.h>
 
+ // Recursividade do movimento para esquerda
 
-int main() {
- 
-    for (int i = 1; i < 2; i++)
-    { for (int j = 0; i < 3; i++) 
-        {
-        printf("Baixo\n");
-        }
-    
-    }
-    int x = 1;
-    int y = 1;
-
-
-    while (x < 2)
-    {
-        x++;
-        while (y < 2)
+    void esquerda(int e) {
+        if (e > 0)
         {
             printf("Esquerda\n");
-            y++; 
+            esquerda(e - 1 );
         }
-        
+    }
+
+// Recursividade do movimento para direita
+
+    void direita(int d) {
+        if (d > 0)
+        {
+            printf("Direita\n");
+            direita(d - 1 );
+        }    
+    }
+// Recursividade do movimento para diagonal
+
+    void diagonal (int Diagonal) {
+        if (Diagonal > 0)
+        {
+            printf("Cima, Direita\n");
+            diagonal(Diagonal - 1 );
+        }
+    }
+
+int main() {
+
+ // Inicialização da variável da Torre
+    int torre = 5;
+
+ // Movimento da torre 5 casas para direita
+ 
+    printf("Torre:\n");
+    direita (torre);
+
+ // Inicialização da variável do Bispo
+    int bispo = 5;
+
+// Movimento do bispo 5 casas para diagonal 
+
+    printf("\nBispo:\n");
+    diagonal (bispo);
+
+// Inicialização da variável da Rainha
+    int rainha = 8;
+
+// Movimento do bispo 8 casas para esquerda 
+
+    printf("\nRainha:\n");
+    esquerda(rainha);
+
+// Movimento do Cavalo 
+
+    printf("\nCavalo:\n");
+    for (int i = 1; i < 2; i++)
+    {for (int f = 1; f < 3; f++)
+    {
+       printf("Cima\n");
     }
     
-        
-    
+    printf("Direita\n");
+    }
     
 
     return 0;
